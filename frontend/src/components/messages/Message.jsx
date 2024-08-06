791aa24ad2e0b92ed3b584b40e812c2737a3bbc8
DhanushAKian <dnush1942@gmail.com>
2024-08-06 16:35:46 +0530

    conversation established 2

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,5 +1,6 @@
 import {useAuthContext} from '../../context/AuthContext';
 import useConversation from '../../zustand/useConversation';
+import { extractTime } from '../utils/extractTime.js';
 
 const Message = ({message}) => {
 const {authUser} =useAuthContext();
@@ -7,7 +8,8 @@ const {selectedConversation}=useConversation();
 const fromMe = message.senderId=== authUser._id;
 const chatClassName = fromMe?'chat-end':'chat-start';
 const profilePic= fromMe? authUser.profilePic :selectedConversation?.profilePic;
-const bubbleBgColor = fromMe? 'bg-blue-500': "";
+const bubbleBgColor = fromMe? 'bg-black': "";
+const formattedTime= extractTime(message.createdAt);
 
 
   return (
@@ -21,8 +23,8 @@ const bubbleBgColor = fromMe? 'bg-blue-500': "";
 
         </div>
       </div>
-     <div className={`chat-bubble text-white bg-gray-500 ${bubbleBgColor}`}>{message.message}</div>
-      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center text-black font-bold">12:34</div>
+     <div className={`chat-bubble text-white bg-gray-500 ${bubbleBgColor}pb-2`}>{message.message}</div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center text-black font-bold">{formattedTime}</div>
     </div>
   )
 }
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,16 +1,24 @@
+import { useEffect, useRef } from "react";
 import Message from "./Message"
 import useGetMessages from "../../hooks/useGetMessages.js"
 
 const Messages = () => {
   const { messages, loading } = useGetMessages();
-  console.log("messages:",messages);
-  return (
+  const lastMessageRef = useRef();
+  useEffect(() => {
+		setTimeout(() => {
+			lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+		}, 100);
+	}, [messages]);
+    return (
     <div className="px-4 flex-1 overflow-auto">
-      {!loading && 
-      messages.length > 0 && 
-      messages.map((message => (
-        < Message key={message._id} message = {message}/>
-      )))}
+      {!loading &&
+				messages.length > 0 &&
+				messages.map((message) => (
+					<div key={message._id} ref={lastMessageRef}>
+						<Message message={message} />
+					</div>
+				))}
       {!loading && messages.length === 0 &&(
         <p className='text-center text-black'>Send a message to start the conversation</p>
       )}
