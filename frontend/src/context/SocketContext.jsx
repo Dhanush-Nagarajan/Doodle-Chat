aea6dce123e3451690b5c8824d6b7f6ec5414f98
MonishwaranB <monishwaran1504@gmail.com>
2024-08-06 17:40:36 +0530

    implementing socket io

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -8,7 +8,6 @@ export const useSocketContext = () => {
 	return useContext(SocketContext);
 };
 
-// eslint-disable-next-line react/prop-types
 export const SocketContextProvider = ({ children }) => {
 	const [socket, setSocket] = useState(null);
 	const [onlineUsers, setOnlineUsers] = useState([]);
@@ -16,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://localhost:5000", {
 				query: {
 					userId: authUser._id,
 				},
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,10 +2,11 @@
 import React from 'react'
 import Sidebar from '../../components/sidebar/Sidebar.jsx'
 import MessageContainer from '../../components/messages/MessageContainer.jsx'
+import '../../App.css'
 
 const Home = () => {
   return (
-    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding'>
+    <div className='mobile flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding'>
       <Sidebar/> 
       <MessageContainer/>    
     </div>
