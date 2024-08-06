56977f9420aa962a43790c013bb1351b8ed296f4
DhanushAKian <dnush1942@gmail.com>
2024-08-06 15:49:38 +0530

    conversation established

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,18 +1,19 @@
 import Message from "./Message"
+import useGetMessages from "../../hooks/useGetMessages.js"
 
 const Messages = () => {
+  const { messages, loading } = useGetMessages();
+  console.log("messages:",messages);
   return (
     <div className="px-4 flex-1 overflow-auto">
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
+      {!loading && 
+      messages.length > 0 && 
+      messages.map((message => (
+        < Message key={message._id} message = {message}/>
+      )))}
+      {!loading && messages.length === 0 &&(
+        <p className='text-center text-black'>Send a message to start the conversation</p>
+      )}
     </div>
   )
 }
